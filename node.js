11cc83734205adc2d545b3c8b0dff61cab5fb965
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2020-08-18 15:04:44 +0200

    Update node.js

--- node.js
+++ node.js
@@ -1,1 +1,27 @@
+FROM node:11.11.0
+LABEL "maintainer"="anna@example.com"
+LABEL "rating"="Five Stars" "class"="First Class"
+
+USER root
+
+ENV AP /data/app
+ENV SCPATH /etc/supervisior/conf.d
+
+RUN apt-get -y update
+
+# The deamons 
+RUN apt-get -y install supervisior
+RUN mkdir -p /var/log/supervisior
+
+# SUpervisior Configuration
+ADD ./supervisord/conf.d/* $SCPATH/
+
+#Application Code
+ADD *.js* $AP/
+
+WORKDIR $AP
+
+RUN npm install
+
+CMD ["supervisord", "-n"]
 
